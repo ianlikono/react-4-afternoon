f60fa24322e60f0b9bae9fafcbf768c58f04a373
ianlikono <ianlikono@gmail.com>
2018-06-12 15:19:50 -0500

    finished assignment

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import Contact from "../Contact/Contact";
+import History from "../History/History";
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        <div className='subnav'>
+        <div className="subnav">
+          <Link to="/about">
+            <h3 className="subnav_links">About</h3>
+          </Link>
+          <Link to="/about/history">
+            <h3 className="subnav_links">History</h3>
+          </Link>
+          <Link to="/about/contact">
+            <h3 className="subnav_links">Contact</h3>
+          </Link>
         </div>
-        <div className='box'>
-        </div>
+        <div className="box" />
+        <Switch>
+          <Route path="/about/history" component={History} />
+          <Route path="/about/contact" component={Contact} />
+          <Route
+            path="/about"
+            render={() => (
+              <div>
+                <h1>About the University</h1>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+                  quis minima commodi dicta quaerat nesciunt maiores
+                  necessitatibus fugit sit id itaque quos, nulla accusamus, quod
+                  perspiciatis assumenda impedit natus alias aperiam veniam
+                  dolore aliquid! Odit temporibus vitae eveniet, corporis
+                  repudiandae quas exercitationem sequi earum, eius aspernatur
+                  quibusdam, eum magni tempora. Aspernatur laboriosam rerum
+                  alias totam praesentium quidem blanditiis at itaque
+                  consequuntur, incidunt quo quod? Nam qui similique maiores
+                  molestiae libero minima, vel harum? Dolorum ducimus dolor
+                  cumque modi distinctio deserunt aspernatur itaque, labore
+                  eaque doloremque. Voluptatum, fugit! Accusantium ullam laborum
+                  in quia nobis, maxime voluptate enim similique tempore totam
+                  doloremque!
+                </p>
+              </div>
+            )}
+          />
+        </Switch>
       </div>
-    )
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './main.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
+import "./main.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById("root")
+);
